070806bb5b83819d3866ea73bef481c463e53fff
Izaura <izahonorato@gmail.com>
2022-04-15 15:22:46 -0300

    ajuste no tamanho da imagem

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,8 +8,8 @@ import onca from '../assets/icons/onca.png';
 export const Login = () => {
     return (
         <div>
-            <img src={onca} alt="onça pintada"/>
-            <form>
+            <img src={onca} alt="onça pintada"  width="40%"/>
+            <form align="center">
                 <input type="text" name="login" placeholder="Usuário"/>
                 <input type="password" name="senha" placeholder="Senha"/>
                 <button>Entrar</button>
