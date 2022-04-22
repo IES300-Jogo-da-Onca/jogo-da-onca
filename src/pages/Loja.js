4ae13b5908073d7110dced111abc52e3189d5a63
Juliana Santos <fernandoguerrierocs@gmail.com>
2022-04-22 19:16:38 -0300

    Estilização da página Loja finalizada, Aguardando comunicação com API e BD para Finalizar detalhes

--- src/pages/Loja.js
+++ src/pages/Loja.js
@@ -1,17 +1,125 @@
 import React from 'react';
 import { Headers } from '../components/Headers';
+import oncaClassica from '../assets/icons/onçaClássicaPeça.svg'
+import dogClassico from '../assets/icons/dogClassicoPeça.svg'
 
 export const Loja= () => {
+
+    //Array Provisário que representa a Array que receberemos do Banco com todas as Skins Disponíveis
+    const skins = [
+        {
+            skinName: 'Onça Clássica',
+            skinImg: oncaClassica,
+            preçoSkin: 8,
+            onça: true
+        },
+        {
+            skinName: 'Onça Clássica',
+            skinImg: oncaClassica,
+            preçoSkin: 8,
+            onça: true
+        },
+        {
+            skinName: 'Onça Clássica',
+            skinImg: dogClassico,
+            preçoSkin: 8,
+            onça: false
+        },
+        {
+            skinName: 'Onça Clássica',
+            skinImg: oncaClassica,
+            preçoSkin: 8,
+            onça: true
+        },
+        {
+            skinName: 'Onça Clássica',
+            skinImg: dogClassico,
+            preçoSkin: 8,
+            onça: false
+        },
+        {
+            skinName: 'Onça Clássica',
+            skinImg: dogClassico,
+            preçoSkin: 8,
+            onça: false
+        },
+        {
+            skinName: 'Onça Clássica',
+            skinImg: oncaClassica,
+            preçoSkin: 8,
+            onça: true
+        },
+        {
+            skinName: 'Onça Clássica',
+            skinImg: dogClassico,
+            preçoSkin: 8,
+            onça: false
+        },
+        {
+            skinName: 'Onça Clássica',
+            skinImg: oncaClassica,
+            preçoSkin: 8,
+            onça: true
+        },
+        {
+            skinName: 'Onça Clássica',
+            skinImg: dogClassico,
+            preçoSkin: 8,
+            onça: false
+        },
+        {
+            skinName: 'Onça Clássica',
+            skinImg: oncaClassica,
+            preçoSkin: 8,
+            onça: true
+        },
+    ]
 
     return (
         <div className='container-generic'>
            <Headers />
            <div className='bodyLoja'>
-                <div className='oncaSkins'>
-                    Onça Works
+                <h2>Onças:</h2>
+                <div className='skinsContainer'>
+                    {
+                        skins.map( s => {
+                            if(s.onça){
+                                return <div className='skin'>
+                                    <img src={s.skinImg}/>
+                                    <div className='skinInfo'>
+                                        <h4>{s.skinName}</h4>
+                                    </div>
+                                    <div className='skinPreco'>
+                                        <span>${s.preçoSkin}</span>
+                                    </div>
+                                    <div>
+                                        <button>Comprar</button>
+                                    </div>
+                                </div>
+                            }
+                        })
+                    }
                 </div>
-                <div className='cachorroSkins'>
-                    Cachorro Works
+                <h2>Cachorros:</h2>
+                <div className='skinsContainer'>
+                    {
+                        skins.map( s => {
+                            if(!s.onça){
+                                return <div className='skin'>
+                                    <img src={s.skinImg}/>
+                                    <div className='skinInfo'>
+                                        <h4>{s.skinName}</h4>
+                                    </div>
+                                    <div className='skinPreco'>
+                                        <span>${s.preçoSkin}</span>
+                                    </div>
+                                    <div>
+                                        <button>Comprar</button>
+                                    </div>
+                                </div>
+                            }
+                        })
+                    }
                 </div>
            </div>
         </div>
