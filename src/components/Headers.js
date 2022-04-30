79fb1aeba6e423fc17f7632c7d85d069ea605ad3
AlexandreAguido <AlexandreAguido@users.noreply.github.com>
2022-04-30 20:39:58 -0300

    adicionar função de logout e ajuste nos links

--- src/components/Headers.js
+++ src/components/Headers.js
@@ -3,20 +3,28 @@ import logo from '../assets/icons/onca-sem-fundo.png';
 import sairIcon from '../assets/icons/sairIcon.png';
 import coinIcon from '../assets/icons/coinIcon.png';
 
-import {useLocation } from 'react-router-dom';
+import {Link, useLocation, useNavigate } from 'react-router-dom';
+import { executaRequisicao } from '../services/api';
 
 export const Headers = props => {
 
     let userName = 'Super User' //Provisório até que tenhamos conexão com o banco
     let minhasMoedas = 0 //Provisório até que tenhamos conexão com o banco
-
+    const navigate = useNavigate()
     const location  = useLocation () 
 
     useEffect(()=>{
         const newMenu = document.querySelector(`a[href='${location.pathname}'] h2`) || document.querySelector(`a[href='${location.pathname}'] p`)
         newMenu.classList.add('selected')
     }, [location])
-
+    const logout = async () => {
+        try {
+            await executaRequisicao('/logout', 'GET')
+            navigate('/')
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return(
         <div className='header'>
             <div className='navContainer'>
@@ -26,27 +34,25 @@ export const Headers = props => {
                 <nav className='navigation'>
                     <ul>
                         <li>
-                            <a href='/home'><h2>Tabuleiro</h2></a>
+                            <Link to='/home'><h2>Tabuleiro</h2></Link>
                         </li>
                         <li>
-                            <a href='/loja'><h2>Loja de Skins</h2></a>
+                            <Link to='/loja'><h2>Loja de Skins</h2></Link>
                         </li>
                         <li>
-                            <a href='/comprarMoedas'><img className='coin' src={coinIcon}/><h2>Comprar Moedas</h2></a>
+                            <Link to='/comprarMoedas'><img className='coin' src={coinIcon}/><h2>Comprar Moedas</h2></Link>
                         </li>
                     </ul>
                 </nav>
             </div>
             <div className='headerRight'>
                 <div className='nameMoney'>
-                    <a href='/perfil'><p id='username'>{userName}</p></a>
+                    <Link to='/perfil'><p id='username'>{userName}</p></Link>
                     <p id='minhasMoedas'>Moedas: ${minhasMoedas}</p>
                 </div>
-                <div className='sair'>
-                    <a href='/'>
+                <div className='sair' onClick={logout}>
                         <img src={sairIcon}/>
                         <p>Sair</p>
-                    </a>
                 </div>
             </div>
         </div>
