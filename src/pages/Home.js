2a07cc06591836130fb323f7d482c76e4d81e975
AlexandreAguido <AlexandreAguido@users.noreply.github.com>
2022-05-03 08:55:15 -0300

    inicio de deseonvolvimento com socket + ajuste tabuleiro

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
 import tabuleiro from '../assets/icons/tabuleiroProvisório.png';
 import { Headers } from '../components/Headers';
+import{io} from "socket.io-client"
+const socket = io("ws://localhost:3001", {withCredentials: true})
 
 export const Home= () => {
+    const [sala, setSala] = useState(null)
+    
+    const criarSala = () => {
+        socket.emit('novaSala')
+    }
 
+    useEffect(() => {
+        socket.on('serverNovaSala', data => setSala(data.idSala))
+
+    }, [socket])
     return (
         <div className='container-generic'>
             <Headers />
             <div className='body'>
                 <div className='optArea'>
-                    <button>Iniciar Jogo!</button>
+                    { !sala && <button onClick={criarSala}>Criar sala!</button> }
+                    { sala && <input disabled value={sala}></input>}
                 </div>
                 <div className='tabuleiroArea'>
                     <img src={tabuleiro}/>
