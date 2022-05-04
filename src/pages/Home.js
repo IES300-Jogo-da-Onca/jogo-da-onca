831255f4170cff443faf3f7f73e5b94e79f8aa81
AlexandreAguido <AlexandreAguido@users.noreply.github.com>
2022-05-04 08:59:21 -0300

    salas para partidas e movimentação de peças

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,68 @@
 import {useState, useEffect} from 'react';
 import tabuleiro from '../assets/icons/tabuleiroProvisório.png';
 import { Headers } from '../components/Headers';
+import { Tabuleiro } from './Tabuleiro';
 import{io} from "socket.io-client"
-const socket = io("ws://localhost:3001", {withCredentials: true})
 
 export const Home= () => {
-    const [sala, setSala] = useState(null)
-    
+    const socket_url = process.env.REACT_APP_WS_URL || "ws://localhost:3001"
+    const [socket, setSocket] = useState(io(socket_url, {withCredentials: true}))
+    const [sala, setSala] = useState('')
+    const [criouSala, setCriouSala] = useState(false)
+    const [isPlaying, setIsPlaying] = useState(false)
+    const [dadosPartida, setDadosPartida] = useState({})
     const criarSala = () => {
         socket.emit('novaSala')
     }
 
-    useEffect(() => {
-        socket.on('serverNovaSala', data => setSala(data.idSala))
+    const joinSala = () => {
+        socket.emit('joinSala', sala)
+    }
 
+    useEffect(() => {
+        
+        socket.on('serverNovaSala', data => {
+            setSala(data.idSala)
+            setCriouSala(true)
+        })
+        socket.on('serverJoinSala', data => {
+            
+        })
+        // TODO: criar evento para sair da sala antes da partida começar
+        socket.on('serverIniciarJogo', data => {
+            console.log('serverIniciarJogo', data)
+            let dadosIniciais = {
+                vetorTabuleiro: data.vetorTabuleiro,
+                ehCachorro: data.ehCachorro,
+                turnoPeca: data.turnoPeca,
+                socket
+            }
+            if(data['skinCachorro']) dadosIniciais['skinCachorro'] = data['skinCachorro']
+            if(data['skinOnca']) dadosIniciais['skinOnca'] = data['skinOnca']
+            if(data['skinTabuleiro']) dadosIniciais['skinTabuleiro'] = data['skinTabuleiro']
+            setDadosPartida(dadosIniciais)
+            setIsPlaying(true)
+
+        })
+        socket.on('error', data => console.error(data))
     }, [socket])
     return (
         <div className='container-generic'>
             <Headers />
             <div className='body'>
                 <div className='optArea'>
-                    { !sala && <button onClick={criarSala}>Criar sala!</button> }
-                    { sala && <input disabled value={sala}></input>}
+                    { !criouSala && !isPlaying && 
+                    <div>
+                        <button  onClick={criarSala}>Criar sala!</button> 
+                        <input value={sala} onChange={(e) => setSala(e.target.value)}/>
+                        <button onClick={joinSala}>Entrar na sala</button>
+                    </div>
+                    }
+                    { criouSala && !isPlaying &&  <div><p>Id da Sala</p><input disabled value={sala}></input></div>}
                 </div>
                 <div className='tabuleiroArea'>
-                    <img src={tabuleiro}/>
+                    {!isPlaying && <img src={tabuleiro}/>}
+                    {isPlaying && <Tabuleiro {...dadosPartida} />           }
                 </div>
             </div>
         </div>
