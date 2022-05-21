59d9082a8d18ecd6164dc87c48bd125496dc3cdb
Izaura <izahonorato@gmail.com>
2022-05-21 13:39:54 -0300

    validação das informações do "cartao" na compra de moedas

---
+++ src/utils/validarCompraMoedas.js
@@ -0,0 +1,23 @@
+//validadores dos inputs da compra de moedas
+
+//validando o tamanho mínimo para o numero do cartao
+const validarNumCartao = (numCartao) => {
+    return numCartao?.toString().length >= 16;
+}
+
+//validando o tamanho mínimo para o nome impresso no cartao 
+const validarnomeCartao = (nomeCartao) => {
+    return nomeCartao?.toString().length >= 5;
+}
+
+//validando tamanho minimo do cvv
+const validarCvv = (cvv) => {
+    return cvv?.toString().length >= 3;
+}
+
+
+export {
+    validarNumCartao,
+    validarnomeCartao,
+    validarCvv,
+}
