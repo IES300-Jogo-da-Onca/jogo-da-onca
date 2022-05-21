70f927d1c2e2d7a5d9fa9fef674bb9f02daad6d8
Izaura <izahonorato@gmail.com>
2022-05-21 18:46:05 -0300

    config inicial página de perfil do usuário

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -6,7 +6,15 @@ export const Perfil= () => {
     return (
         <div className='container-generic'>
            <Headers />
-           <div className='body'>
+           <div className='container-perfil'>
+               <div className='container-skins'>
+                   <h3>Skins</h3>
+                   <h4>Onça</h4>
+                   <h4>Cachorro</h4>
+                </div>
+            <div className='container-moedas'>
+                <h3>Moedas</h3>
+            </div>
 
            </div>
         </div>
