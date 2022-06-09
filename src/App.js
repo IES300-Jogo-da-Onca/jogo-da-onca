724e92e4e5ef6dc8003247e7e41999ae8d81450b
Fernando Guerriero Cardoso da Silva <fernandoguerriero@la.foxconn.com>
2022-06-09 14:13:16 -0300

    Segurança nas rotas - Apenas login e cadastro são publicos, demais rotas é preciso estar logado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, {useContext, useEffect, useState} from "react";
 import './styles/app.scss';
 import {Login} from './pages/Login';
-import { Route, Routes} from 'react-router-dom';
+import { Route, Routes, useNavigate, useLocation, Navigate} from 'react-router-dom';
 import { Cadastro } from './pages/Cadastro';
 import { Home } from "./pages/Home";
 import { Loja } from './pages/Loja';
@@ -9,20 +9,52 @@ import { ComprarMoedas } from "./pages/ComprarMoedas";
 import { Perfil } from "./pages/Perfil";
 import {Tabuleiro} from "./pages/Tabuleiro"
 import UserProvider from "./context/UserContext";
-function App() {
+import RequireAuth from './components/RequireAuth'
+
+const App = () => {
+
+  useEffect(()=>{
+    //console.log("Teste: ", authenticatted)
+  },[])
+  
   return (
     <UserProvider>
       <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/cadastro" element={<Cadastro />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/loja" element={<Loja />} />
-          <Route path="/comprarMoedas" element={<ComprarMoedas />} />
-          <Route path="/perfil" element={<Perfil />} />
-          <Route path="/tabuleiro" element={<Tabuleiro />} />
+          <Route path="/" element={
+            localStorage.getItem('userData') ? <Navigate to="/home" replace /> : <Login /> 
+          } />
+
+          <Route path="/cadastro" element={
+            localStorage.getItem('userData') ? <Navigate to="/home" replace /> : <Cadastro/>
+          } />
+          
+          <Route
+            path="/home"
+            element={
+              <RequireAuth>
+                <Home />
+              </RequireAuth>
+            }
+          />
+          <Route path="/loja" element={
+            <RequireAuth>
+              <Loja />
+            </RequireAuth>
+          }/>
+          <Route path="/comprarMoedas" element={
+            <RequireAuth>
+              <ComprarMoedas />
+            </RequireAuth>
+          }/>
+          <Route path="/perfil" element={
+            <RequireAuth>
+              <Perfil />
+            </RequireAuth>
+          }/>
+          <Route path="/tabuleiro" element={<Tabuleiro />}/>
       </Routes>
     </UserProvider>
   );
 }
 
-export default App;
+export default App
--- src/pages/Loja.js
+++ src/pages/Loja.js
@@ -2,25 +2,56 @@ import React, { useEffect, useState, useContext } from 'react';
 import { Headers } from '../components/Headers';
 import { UserContext } from '../context/UserContext';
 import { executaRequisicao } from '../services/api';
+import {Modal} from 'react-bootstrap';
+import pagIcon from '../assets/icons/pag-sucesso.png';
+import Spinner from 'react-bootstrap/Spinner';
 
 export const Loja= () => {
 
+    //Skin a ser comprada
+    const [skinToBuy, setSkinToBuy] = useState({})
     const [skins, setSkins] = useState([])
+    const [showModal, setShowModal] = useState(false);
+    const [confirmedBuy, setConfirmedBuy] = useState(false);
+    const [onLoading, setOnLoading] = useState(false)
     const {userInfo, setUserInfo} = useContext(UserContext)
     const getSkinsLoja = () => {
         return executaRequisicao('/loja', 'GET')
     }
 
+    const handleBuySkin = buy => {
+        if(buy) {
+            setOnLoading(true)
+            buySkin(skinToBuy.idSkin)
+            return
+        }
+        setSkinToBuy({})
+        setShowModal(false)
+    }
+
     const buySkin = (idSkinToBuy) => {
         executaRequisicao('/comprarskin', 'POST', { idSkin: idSkinToBuy })
         .then( res => {
-           if(res.data.comprouSkin){
-               alert('Compra realizada com sucesso')
+            console.log("Response: ", res)
+            if(res.data.comprouSkin){
+               console.log("Response: ", res, '\nData: ', res.data)
+               setOnLoading(false)
+               setConfirmedBuy(true)
+               setTimeout(() => {
+                   setConfirmedBuy(false)
+                   setSkinToBuy({})
+                   setShowModal(false)
+               }, 2000)
                setSkins(skins.filter(skin => skin.idSkin !== idSkinToBuy))
                setUserInfo({...userInfo, moedas: res.data.moedas})
-           }
+            }
         })
-        .catch( err => window.alert("Erro na requisição.\nContate Administrador."))
+        .catch( err => {
+            window.alert(err.response.data.mensagem)
+            setShowModal(false)
+            setOnLoading(false)
+            setSkinToBuy({})
+        })
     }
     
     useEffect(() => {
@@ -48,7 +79,13 @@ export const Loja= () => {
                                     </div>
                                     <div>
                                         <button onClick={() => {
-                                            buySkin(s.idSkin)
+                                            setSkinToBuy({
+                                                'idSkin': s.idSkin,
+                                                'skinImg': s.skinImg,
+                                                'skinName': s.skinName,
+                                                'precoSkin': s.precoSkin
+                                            })
+                                            setShowModal(true)
                                         }}>Comprar</button>
                                     </div>
                                 </div>
@@ -71,7 +108,14 @@ export const Loja= () => {
                                     </div>
                                     <div>
                                         <button onClick={() => {
-                                            buySkin(s.idSkin)
+                                            //buySkin(s.idSkin)
+                                            setSkinToBuy({
+                                                'idSkin': s.idSkin,
+                                                'skinImg': s.skinImg,
+                                                'skinName': s.skinName,
+                                                'precoSkin': s.precoSkin
+                                            })
+                                            setShowModal(true)
                                         }}>Comprar</button>
                                     </div>
                                 </div>
@@ -80,6 +124,41 @@ export const Loja= () => {
                     }
                 </div>
             </div>
+            <Modal className="confirm-buy" show={showModal} onHide={() => setShowModal(false)}
+                backdrop="static" keyboard={false}>
+                <Modal.Header>
+                    <h3>Confirmar Aquisição</h3>
+                </Modal.Header>
+                <Modal.Body>
+                    {onLoading ?
+                        <Spinner animation="border" variant="success" />
+                        : confirmedBuy ?
+                            <div>
+                                <h1>Compra realizada com sucesso!</h1>
+                                <img src={pagIcon}></img> 
+                            </div> 
+                            :
+                            <div>
+                                <p>Deseja realmente comprar a seguinte skin?</p>
+                                <div className='skin' key={skinToBuy.idSkin}>
+                                    <img src={skinToBuy.skinImg}/>
+                                    <div className='skinInfo'>
+                                        <h4>{skinToBuy.skinName}</h4>
+                                    </div>
+                                    <div className='skinPreco'>
+                                        <span>${skinToBuy.precoSkin}</span>
+                                    </div>
+                                </div>
+                                <button className="btn-confirma" variant="secondary" onClick={() => handleBuySkin(true)}>
+                                    Confirmar
+                                </button>
+                                <button className="btn-cancela" variant="secondary" onClick={() => handleBuySkin(false)}>
+                                    Cancelar
+                                </button>
+                            </div>
+                    }
+                </Modal.Body>
+            </Modal>
         </div>
     )
 }
